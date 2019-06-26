92c39b9975cd8648f70d2905d9850314e066ded1
Minh Hung <ngominhhung97@gmail.com>
2019-06-26 14:46:54 +0700

    add readme

--- Bookstore/src/main/java/com/minhhung/Bookstore/Models/Book.java
+++ Bookstore/src/main/java/com/minhhung/Bookstore/Models/Book.java
@@ -1,4 +1,41 @@
 package com.minhhung.Bookstore.Models;
 
+
 public class Book {
+
+    private int id;
+
+    private String name;
+
+    private int year;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Book %d year %d name %s]", id, year, name);
+    }
+
 }
